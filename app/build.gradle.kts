601576a0788bd03f23d43032dd73a932d398a461
ali <daulay.alidev@gmail.com>
2023-11-19 19:43:16 +0700

    remove idea

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ghifarix.simulasi_uang"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ghifarix.simulasi_uang"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
