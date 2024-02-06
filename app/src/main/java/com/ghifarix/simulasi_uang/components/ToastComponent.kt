7c8db6257615f511cee6e6b57d28f37c0ae7ce46
ali <daulay.alidev@gmail.com>
2024-02-06 14:25:03 +0700

    - show pdf after created.

--- app/src/main/java/com/ghifarix/simulasi_uang/components/ToastComponent.kt
+++ app/src/main/java/com/ghifarix/simulasi_uang/components/ToastComponent.kt
@@ -2,24 +2,27 @@ package com.ghifarix.simulasi_uang.components
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Environment
 import android.widget.Toast
-import com.ghifarix.simulasi_uang.R
+import androidx.core.content.FileProvider
 import com.ghifarix.simulasi_uang.SingletonModel
 import com.ghifarix.simulasi_uang.extensions.generatePdf
 import java.io.File
 
 fun toastGeneratePdfSuccess(context: Context): String {
     val pdfName = context.generatePdf(pdf = SingletonModel.getInstance().getPdf())
-    val infoName = "${context.getString(R.string.file_name)} $pdfName"
-    Toast.makeText(context, infoName, Toast.LENGTH_SHORT).show()
-    return infoName
+    return pdfName ?: ""
 }
 
 fun openPdf(context: Context, fileName: String) {
-    val file = File(Environment.DIRECTORY_DOCUMENTS, fileName)
-    val path = Uri.fromFile(file)
+    val downloadsDirectory =
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+    val file = File(downloadsDirectory, "$fileName.pdf")
+    val path = FileProvider.getUriForFile(
+        context,
+        context.applicationContext.packageName + ".provider",
+        file
+    )
     val intent = Intent(Intent.ACTION_VIEW)
     intent.setDataAndType(path, "application/pdf")
     try {
